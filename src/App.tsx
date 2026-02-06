cc5dba76d0f51e2eee702b40e3cef380053805e1
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2026-02-06 05:40:20 -0300

    despliegue del about me

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,9 @@ function App() {
         {/* MODULO 2 */}
         <div className="mod-2">
           <h3>About Me</h3>
-          <p></p>
+          <p>I am a <span>University Technician in Programming</span> (UTN) passionate about Scalable Architecture and Intuitive Design.
+          I combine <span>creativity</span> with <span>logic</span> to build <span>Full Stack Web Apps</span> (React, Node.js) and <span>Video Games</span> (Unity). My focus is always on writing clean code and creating meaningful experiences. I am <span>proactive</span> and ready to bring ideas to life
+          </p>
         </div>
         {/* MODULO 3 */}
         <div className="mod-3">3</div>
